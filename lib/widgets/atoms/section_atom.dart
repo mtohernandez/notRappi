f4b1b1367acdac6451bb3af7d7e7e384fabc67f2
mtohernandez <m4teoh3rnandez@outlook.com>
2022-12-10 20:48:56 -0500

    Add initial state of section atom widget

---
+++ lib/widgets/atoms/section_atom.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SectionAtom extends StatelessWidget {
+  final String title;
+  final bool isGrid;
+
+  const SectionAtom(this.title, this.isGrid, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
