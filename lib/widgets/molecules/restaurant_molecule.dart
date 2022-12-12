9f12492300c65165c3c4db732004ec6a3d3eb318
dMuvdi <98854794+dMuvdi@users.noreply.github.com>
2022-12-12 18:00:26 -0500

    refactored restaurant molecule

--- lib/widgets/molecules/restaurant_molecule.dart
+++ lib/widgets/molecules/restaurant_molecule.dart
@@ -25,33 +25,94 @@ class RestaurantMolecule extends StatefulWidget {
   State<RestaurantMolecule> createState() => _RestaurantMoleculeState();
 }
 
+
+//!widget.isSearch ? SearchWidget() : NotSearchWdiget() final result
+
 class _RestaurantMoleculeState extends State<RestaurantMolecule> {
-  @override
-  Widget build(BuildContext context) {
-    return !widget.isSearch ? GestureDetector(
-      onTap: () => {},
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height / 7.8,
-        child: Row(
-          children: [
-            Container(
-              width: 170.0,
-              height: 80.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                    widget.imageUrl
-                  )
-                )
-              ),
-            ),
-            const SizedBox(width: 7.5,),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Column(
+
+  Widget _buildMiddleRow(double rating, int ratingAmount, int estimatedTime, double distance){
+    return Row(
+      children: [
+        SvgPicture.asset(
+          'assets/icons/starIcon.svg',
+          width: 20.0,
+          height: 20.0,
+        ),
+        Text(
+          ' $rating', 
+          style: const TextStyle(
+            fontSize: 10.0, 
+            fontWeight: FontWeight.bold,
+            color: Colors.red
+          ),
+        ),
+        Text(' ($ratingAmount)', style: const TextStyle(fontSize: 10.0, color: Color.fromARGB(255, 102, 102, 102)),),
+        const SizedBox(width: 5.0,),
+        SvgPicture.asset(
+          'assets/icons/timerIcon.svg',
+          width: 20.0,
+          height: 20.0,
+        ),
+        Text(' $estimatedTime min ', style: const TextStyle(fontSize: 10.0),),
+        Container(
+          width: 4.5,
+          height: 4.5,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black,
+          ),
+        ),
+        Text(' $distance km', style: const TextStyle(fontSize: 10.0),)
+      ],
+    );
+  }
+
+  Widget buildDiscountItem(){
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Text(
+          '20% OFF TODAY',
+          style: TextStyle(
+            fontSize: 10.0,
+            color: Colors.white
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRestaurantImage(bool isSearch){
+    return Container(
+      width: !isSearch ? MediaQuery.of(context).size.width / 2.11 : MediaQuery.of(context).size.width * 1.11,
+      height: !isSearch ? MediaQuery.of(context).size.height / 9.65 : MediaQuery.of(context).size.height / 10.03,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            widget.imageUrl
+          )
+        )
+      ),
+    );
+  }
+
+  Widget buildSearch(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildRestaurantImage(widget.isSearch),
+        Padding(
+          padding: const EdgeInsets.only(top: 5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
@@ -62,161 +123,51 @@ class _RestaurantMoleculeState extends State<RestaurantMolecule> {
                     ),
                   ),
                   const SizedBox( height: 5.0,),
-                  Row(
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/starIcon.svg',
-                        width: 20.0,
-                        height: 20.0,
-                      ),
-                      Text(
-                        ' ${widget.raiting}', 
-                        style: const TextStyle(
-                          fontSize: 10.0, 
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red
-                        ),
-                      ),
-                      Text(' (${widget.raitingAmount})', style: const TextStyle(fontSize: 10.0, color: Color.fromARGB(255, 102, 102, 102)),),
-                      const SizedBox(width: 5.0,),
-                      SvgPicture.asset(
-                        'assets/icons/timerIcon.svg',
-                        width: 20.0,
-                        height: 20.0,
-                      ),
-                      Text(' ${widget.estimatedTime} min ', style: const TextStyle(fontSize: 10.0),),
-                      Container(
-                        width: 4.5,
-                        height: 4.5,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(' ${widget.distance} km', style: const TextStyle(fontSize: 10.0),)
-                    ],
-                  ),
-                  const SizedBox( height: 5.0,),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                          '20% OFF TODAY',
-                          style: TextStyle(
-                            fontSize: 10.0,
-                            color: Colors.white
-                          ),
-                        ),
-                    ),
-                  )
+                  _buildMiddleRow(widget.raiting, widget.raitingAmount, widget.estimatedTime, widget.distance),
                 ],
               ),
-            )
-          ],
-        ),
-      ),
-    ) :  Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12.0),
-      child: GestureDetector(
-        onTap: () => {},
-        child: SizedBox(
-          width: 350.0,
-          height: 120.0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 400.0,
-                height: 70.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      widget.imageUrl
-                    )
-                  )
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.restaurantName,
-                          style: const TextStyle(
-                            fontSize: 13.5,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox( height: 5.0,),
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/starIcon.svg',
-                              width: 20.0,
-                              height: 20.0,
-                            ),
-                            Text(
-                              ' ${widget.raiting}', 
-                              style: const TextStyle(
-                                fontSize: 12.0, 
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red
-                              ),
-                            ),
-                            Text(' (${widget.raitingAmount})', style: const TextStyle(fontSize: 12.0, color: Color.fromARGB(255, 102, 102, 102)),),
-                            const SizedBox(width: 5.0,),
-                            SvgPicture.asset(
-                              'assets/icons/timerIcon.svg',
-                              width: 20.0,
-                              height: 20.0,
-                            ),
-                            Text(' ${widget.estimatedTime} min ', style: const TextStyle(fontSize: 12.0),),
-                            Container(
-                              width: 4.5,
-                              height: 4.5,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.black,
-                              ),
-                            ),
-                            Text(' ${widget.distance} km', style: const TextStyle(fontSize: 12.0),)
-                          ],
-                        ),
-                      ],
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Text(
-                            '20% OFF TODAY',
-                            style: TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.white
-                            ),
-                          ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              buildDiscountItem()
             ],
           ),
         ),
-      ),
+      ],
+    );
+  }
+
+  Widget buildNotSearch(){
+    return Row(
+      children: [
+        _buildRestaurantImage(widget.isSearch),
+        const SizedBox(width: 7.5,),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.restaurantName,
+                style: const TextStyle(
+                  fontSize: 13.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox( height: 5.0,),
+              _buildMiddleRow(widget.raiting, widget.raitingAmount, widget.estimatedTime, widget.distance),
+              const SizedBox( height: 5.0,),
+              buildDiscountItem()
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: !widget.isSearch ? MediaQuery.of(context).size.width : MediaQuery.of(context).size.width - 1,
+      height: !widget.isSearch ? MediaQuery.of(context).size.height / 7.8 : MediaQuery.of(context).size.height / 6.01,
+      child: !widget.isSearch ? buildNotSearch() : buildSearch(),
     );
   }
 }
